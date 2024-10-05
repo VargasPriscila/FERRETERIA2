db32c674b8165bc1aeeb97d74d0558ec02605a77
VargasPriscila <m.priscila.v@gmail.com>
2024-10-05 17:25:22 -0300

    proyecto documentado

--- sistema/miAplicacion/views.py
+++ sistema/miAplicacion/views.py
@@ -1,3 +1,18 @@
+"""
+Vistas para la gestión del sistema de hardware.
+
+Este archivo contiene las vistas utilizadas en la aplicación para gestionar los distintos
+módulos como proveedores, categorías, productos, ventas y clientes. Las vistas proporcionan
+funcionalidad para listar, crear, actualizar, eliminar y gestionar objetos en la base de datos.
+
+Las vistas son:
+    - index: Renderiza la página principal.
+    - Proveedores: CRUD de proveedores.
+    - Categorías: CRUD de categorías.
+    - Productos: CRUD de productos.
+    - Ventas: Gestión de ventas y detalles de ventas.
+    - Clientes: CRUD de clientes.
+"""
 # views.py
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Categoria, Proveedor, Producto, Venta, DetalleVenta, Cliente
@@ -8,16 +23,41 @@ from django.http import JsonResponse
 
 
 
-
 def index(request):
+    """
+    Renderiza la página principal del sitio.
+    """
     return render(request, "index.html")
 
-# Proveedor
+
+
+# ------------------------------ Proveedores -------------------------------------------------
+
 def proveedor_list(request):
+    """
+    Muestra una lista de todos los proveedores disponibles.
+
+    Args:
+        request: El objeto de solicitud HTTP.
+
+    Returns:
+        Renderiza la plantilla 'proveedor_list.html' con la lista de proveedores.
+    """
     proveedores = Proveedor.objects.all()
     return render(request, 'proveedores/proveedor_list.html', {'proveedores': proveedores})
 
 def proveedor_create(request):
+    """
+    Crea un nuevo proveedor. Si se envía una solicitud POST válida,
+    se guarda el proveedor en la base de datos.
+
+    Args:
+        request: El objeto de solicitud HTTP.
+
+    Returns:
+        Renderiza el formulario para crear un proveedor o redirige
+        a la lista de proveedores si el formulario es válido.
+    """
     if request.method == 'POST':
         form = ProveedorForm(request.POST)
         if form.is_valid():
@@ -28,6 +68,17 @@ def proveedor_create(request):
     return render(request, 'proveedores/proveedor_form.html', {'form': form})
 
 def proveedor_update(request, pk):
+    """
+    Actualiza un proveedor existente basado en el identificador proporcionado.
+
+    Args:
+        request: El objeto de solicitud HTTP.
+        pk: El identificador del proveedor.
+
+    Returns:
+        Renderiza el formulario de actualización de proveedor o redirige
+        a la lista de proveedores si el formulario es válido.
+    """
     proveedor = get_object_or_404(Proveedor, pk=pk)
     if request.method == 'POST':
         form = ProveedorForm(request.POST, instance=proveedor)
@@ -39,18 +90,52 @@ def proveedor_update(request, pk):
     return render(request, 'proveedores/proveedor_form.html', {'form': form})
 
 def proveedor_delete(request, pk):
+    """
+    Elimina un proveedor existente basado en el identificador proporcionado.
+    
+    Args:
+        request: El objeto de solicitud HTTP.
+        pk: El identificador del proveedor.
+    
+    Returns:
+        Renderiza una confirmación de eliminación o redirige a la lista 
+        de proveedores si se confirma la eliminación.
+    """
     proveedor = get_object_or_404(Proveedor, pk=pk)
     if request.method == 'POST':
         proveedor.delete()
         return redirect('proveedor_list')
     return render(request, 'proveedores/proveedor_confirm_delete.html', {'proveedor': proveedor})
 
-# Categoria
+
+
+# ------------------------------ Categorías --------------------------------------------------
+
 def categoria_list(request):
+    """
+    Muestra una lista de todas las categorías disponibles.
+    
+    Args:
+        request: El objeto de solicitud HTTP.
+    
+    Returns:
+        Renderiza la plantilla 'categoria_list.html' con la lista de categorías.
+    """
     categorias = Categoria.objects.all()
     return render(request, 'categorias/categoria_list.html', {'categorias': categorias})
 
 def categoria_create(request):
+    """
+    Crea una nueva categoría. Si se envía una solicitud POST válida, 
+    se guarda la categoría en la base de datos.
+    
+    Args:
+        request: El objeto de solicitud HTTP.
+    
+    Returns:
+        Renderiza el formulario para crear una categoría o redirige a la lista 
+        de categorías si el formulario es válido.
+    """
     if request.method == 'POST':
         form = CategoriaForm(request.POST)
         if form.is_valid():
@@ -61,6 +146,17 @@ def categoria_create(request):
     return render(request, 'categorias/categoria_form.html', {'form': form})
 
 def categoria_update(request, pk):
+    """
+    Actualiza una categoría existente basada en el identificador proporcionado.
+    
+    Args:
+        request: El objeto de solicitud HTTP.
+        pk: El identificador de la categoría.
+    
+    Returns:
+        Renderiza el formulario de actualización de categoría o redirige 
+        a la lista de categorías si el formulario es válido.
+    """
     categoria = get_object_or_404(Categoria, pk=pk)
     if request.method == 'POST':
         form = CategoriaForm(request.POST, instance=categoria)
@@ -72,6 +168,17 @@ def categoria_update(request, pk):
     return render(request, 'categorias/categoria_form.html', {'form': form})
 
 def categoria_delete(request, pk):
+    """
+    Elimina una categoría existente basada en el identificador proporcionado.
+    
+    Args:
+        request: El objeto de solicitud HTTP.
+        pk: El identificador de la categoría.
+    
+    Returns:
+        Renderiza una confirmación de eliminación o redirige a la lista de 
+        categorías si se confirma la eliminación.
+    """
     categoria = get_object_or_404(Categoria, pk=pk)
     if request.method == 'POST':
         categoria.delete()
@@ -81,15 +188,33 @@ def categoria_delete(request, pk):
 
 
 
-"""-------------------------------------------------------------------------------------------------------------------------------------------------
-"""
-
+# ------------------------------ Productos -------------------------------------------------
 
 def productos_list(request):
+    """
+    Muestra una lista de todos los productos disponibles.
+    
+    Args:
+        request: El objeto de solicitud HTTP.
+    
+    Returns:
+        Renderiza la plantilla 'productos_list.html' con la lista de productos.
+    """
     productos = Producto.objects.all()
     return render(request, 'productos/productos_list.html', {'productos': productos})
 
 def productos_create(request):
+    """
+    Crea un nuevo producto. Si se envía una solicitud POST válida, 
+    se guarda el producto en la base de datos.
+    
+    Args:
+        request: El objeto de solicitud HTTP.
+    
+    Returns:
+        Renderiza el formulario para crear un producto o redirige a la lista 
+        de productos si el formulario es válido.
+    """
     if request.method == 'POST':
         form = ProductoForm(request.POST)
         if form.is_valid():
@@ -100,6 +225,17 @@ def productos_create(request):
     return render(request, 'productos/productos_form.html', {'form': form})
 
 def productos_update(request, pk):
+    """
+    Actualiza un producto existente basado en el identificador proporcionado.
+    
+    Args:
+        request: El objeto de solicitud HTTP.
+        pk: El identificador del producto.
+    
+    Returns:
+        Renderiza el formulario de actualización de producto o redirige a la lista 
+        de productos si el formulario es válido.
+    """
     producto = get_object_or_404(Producto, pk=pk)
     if request.method == 'POST':
         form = ProductoForm(request.POST, instance=producto)
@@ -111,6 +247,17 @@ def productos_update(request, pk):
     return render(request, 'productos/productos_form.html', {'form': form})
 
 def productos_delete(request, pk):
+    """
+    Elimina un producto existente basado en el identificador proporcionado.
+    
+    Args:
+        request: El objeto de solicitud HTTP.
+        pk: El identificador del producto.
+    
+    Returns:
+        Renderiza una confirmación de eliminación o redirige a la lista de 
+        productos si se confirma la eliminación.
+    """
     producto = get_object_or_404(Producto, pk=pk)
     if request.method == 'POST':
         producto.delete()
@@ -118,19 +265,37 @@ def productos_delete(request, pk):
     return render(request, 'productos/productos_confirm_delete.html', {'producto': producto})
 
 
-"""-------------------------------------------------------------------------------------------------------------------------------------------------
-"""
 
-# Ventas
+
+# ------------------------------ Ventas --------------------------------------------------
 
 # Vista para listar ventas
 def venta_lista(request):
+    """
+    Muestra una lista de todas las ventas no anuladas.
+    
+    Args:
+        request: El objeto de solicitud HTTP.
+    
+    Returns:
+        Renderiza la plantilla 'venta_lista.html' con la lista de ventas.
+    """
     ventas = Venta.objects.filter(anulada=False).all()
     return render(request, 'ventas/venta_lista.html', {'ventas': ventas})
 
 # Vista para agregar una venta
 @transaction.atomic
 def venta_agregar(request):
+    """
+    Crea una nueva venta, incluyendo el detalle de los productos seleccionados.
+    
+    Args:
+        request: El objeto de solicitud HTTP.
+    
+    Returns:
+        Renderiza el formulario para crear una venta o redirige a la lista 
+        de ventas si el formulario es válido.
+    """
     if request.method == 'POST':
         venta_form = VentaForm(request.POST)
 
@@ -200,16 +365,35 @@ def obtener_precio_producto(request, producto_id):
     producto = get_object_or_404(Producto, id=producto_id)
     return JsonResponse({'precio': str(producto.precio)})
 
-"""-------------------------------------------------------------------------------------------------------------------------------------------------
-"""
 
+
+# ------------------------------ Clientes --------------------------------------------------
 # Vista para listar clientes
 def cliente_lista(request):
+    """
+    Muestra una lista de todos los clientes registrados en el sistema.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP recibida.
+
+    Returns:
+        HttpResponse: Renderiza la plantilla 'cliente_lista.html' con el contexto de todos los clientes.
+    """
     clientes = Cliente.objects.all()
     return render(request, 'clientes/cliente_lista.html', {'clientes': clientes})
 
 # Vista para agregar cliente
 def cliente_agregar(request):
+    """
+    Muestra un formulario para agregar un nuevo cliente y procesa los datos si se envían.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP recibida.
+
+    Returns:
+        HttpResponse: Si el formulario es válido, redirige a 'cliente_lista'. 
+                      De lo contrario, renderiza la plantilla 'cliente_form.html' con el formulario vacío o con errores.
+    """
     if request.method == 'POST':
         form = ClienteForm(request.POST)
         if form.is_valid():
@@ -221,6 +405,17 @@ def cliente_agregar(request):
 
 # Vista para editar cliente
 def cliente_editar(request, pk):
+    """
+    Muestra un formulario para editar un cliente existente y procesa los datos si se envían.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP recibida.
+        pk (int): El identificador primario del cliente a editar.
+
+    Returns:
+        HttpResponse: Si el formulario es válido, redirige a 'cliente_lista'. 
+                      De lo contrario, renderiza la plantilla 'cliente_form.html' con el formulario y datos existentes o errores.
+    """
     cliente = get_object_or_404(Cliente, pk=pk)
     if request.method == 'POST':
         form = ClienteForm(request.POST, instance=cliente)
@@ -233,6 +428,17 @@ def cliente_editar(request, pk):
 
 # Vista para eliminar cliente
 def cliente_eliminar(request, pk):
+    """
+    Muestra una página de confirmación para eliminar un cliente y procesa la eliminación si se confirma.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP recibida.
+        pk (int): El identificador primario del cliente a eliminar.
+
+    Returns:
+        HttpResponse: Si se confirma la eliminación, redirige a 'cliente_lista'. 
+                      De lo contrario, renderiza la plantilla 'cliente_confirm_delete.html' para la confirmación de eliminación.
+    """
     cliente = get_object_or_404(Cliente, pk=pk)
     if request.method == 'POST':
         cliente.delete()
--- sistema/sistema/settings.py
+++ sistema/sistema/settings.py
@@ -125,8 +125,9 @@ USE_TZ = False
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'miAplicacion/static'),
 ]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
